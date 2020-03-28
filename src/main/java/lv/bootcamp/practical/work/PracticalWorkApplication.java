4f43b1cb8a70b42c79b8ba7642bd54aac0b74c83
LindaP <nightberii@gmail.com>
2020-03-28 12:43:00 +0200

    Movies project

---
+++ src/main/java/lv/bootcamp/practical/work/PracticalWorkApplication.java
@@ -0,0 +1,13 @@
+package lv.bootcamp.practical.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/bootcamp/practical/work/PracticalWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.bootcamp.practical.work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
