9702bb7a90b8f622ae6c4f59b7703b8c8c617082
KriTil <tiltina.kristine@inbox.lv>
2020-03-30 20:07:30 +0300

    sql file for creating database

---
+++ src/main/resources/db.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS movies;
+
+CREATE TABLE IF NOT EXISTS categories (
+  id INT(15)  AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+);
+
+CREATE TABLE IF NOT EXISTS movies (
+  id INT(15) AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(150),
+  year SMALLINT(4),
+  description TEXT(500),
+  rating FLOAT(2,1),
+  link_imdb VARCHAR(300),
+  link_poster VARCHAR(300),
+  category INT(15) NOT NULL,
+  FOREIGN KEY (category) REFERENCES categories(id)
+);
+
+
