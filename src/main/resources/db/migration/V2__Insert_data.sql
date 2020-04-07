247903b9738a1771bef92501b003d8e99ab91b59
KriTil <tiltina.kristine@inbox.lv>
2020-04-07 21:50:51 +0300

    flyway version

---
+++ src/main/resources/db/migration/V2__Insert_data.sql
@@ -0,0 +1,497 @@
+	INSERT INTO categories
+    VALUES (1, "Action");
+
+	INSERT INTO categories
+    VALUES (2, "Animation");
+
+	INSERT INTO categories
+    VALUES (3, "Comedy");
+
+    INSERT INTO categories
+    VALUES (4, "Drama");
+
+    INSERT INTO categories
+    VALUES (5, "Fantasy");
+
+    INSERT INTO categories
+    VALUES (6, "Thriller");
+
+
+	INSERT INTO movies
+	VALUES (1,
+	"The Dark Knight",
+	2008,
+	"When the menace known as the Joker wreaks havoc and chaos on the people
+	of Gotham, Batman must accept one of the greatest psychological and physical
+	tests of his ability to fight injustice.",
+	9.0,
+    "https://www.imdb.com/title/tt0468569/",
+	"https://www.imdb.com/title/tt0468569/mediaviewer/rm4023877632",
+	1
+    );
+
+	INSERT INTO movies
+	VALUES (2,
+	"The Mountain II",
+	2016,
+	"In a desolate war zone where screams of the innocent echo, seven Maroon
+	Berets will dance with death on the very line between disaster and valor.",
+	8.9,
+	"https://www.imdb.com/title/tt5813916/",
+	"https://www.imdb.com/title/tt5813916/mediaviewer/rm2982352640",
+    1
+    );
+
+    INSERT INTO movies
+	VALUES (3,
+	"The Gentlemen",
+	2019,
+	"An American expat tries to sell off his highly profitable marijuana empire
+	in London, triggering plots, schemes, bribery and blackmail in an attempt to
+	steal his domain out from under him.",
+	8.0,
+	"https://www.imdb.com/title/tt8367814/",
+	"https://www.imdb.com/title/tt8367814/mediaviewer/rm1937148929",
+    1
+    );
+
+	INSERT INTO movies
+	VALUES (4,
+	"The Tale of the Sparrow",
+	2015,
+	"Follows the story of a dishonest 20 year old boy named Anthony. Anthony
+	is given an opportunity in which he is able to portray himself as a vigilante
+	named The Sparrow.",
+	8.6,
+	"https://www.imdb.com/title/tt7070022/",
+	"https://www.imdb.com/title/tt7070022/mediaviewer/rm44576256",
+    1
+    );
+
+	INSERT INTO movies
+	VALUES (5,
+	"B.D. in alert",
+	1971,
+	"In 1971 three inept petty Romanian criminals are released from prison but
+	find themselves at the core of two money laundering and burglary cases.",
+	8.7,
+	"https://www.imdb.com/title/tt0123390/",
+	"https://www.imdb.com/title/tt0123390/mediaviewer/rm4258326016",
+    1
+    );
+
+	INSERT INTO movies
+	VALUES (6,
+	"The Lion King",
+	1994,
+	"A Lion cub crown prince is tricked by a treacherous uncle into thinking he
+	caused his father's death and flees into exile in despair, only to learn in
+	adulthood his identity and his responsibilities.",
+	8.5,
+	"https://www.imdb.com/title/tt0110357/",
+	"https://www.imdb.com/title/tt0110357/mediaviewer/rm3272938240",
+	2
+	);
+
+	INSERT INTO movies
+	VALUES (7,
+	"Spirited Away",
+	2001,
+	"During her family's move to the suburbs, a sullen 10-year-old girl wanders into a
+	world ruled by gods, witches, and spirits, and where humans are changed into beasts.",
+	8.6,
+	"https://www.imdb.com/title/tt0245429/",
+	"https://www.imdb.com/title/tt0245429/mediaviewer/rm1173815808",
+	2
+	);
+
+	INSERT INTO movies
+	VALUES (8,
+	"Your Name.",
+	2016,
+	"Two strangers find themselves linked in a bizarre way. When a connection forms, will
+	distance be the only thing to keep them apart?",
+	8.4,
+	"https://www.imdb.com/title/tt5311514/",
+	"https://www.imdb.com/title/tt5311514/mediaviewer/rm3216783616",
+	2
+	);
+
+	INSERT INTO movies
+	VALUES (9,
+	"Coco",
+	2017,
+	"Aspiring musician Miguel, confronted with his family's ancestral ban on music, enters the
+	Land of the Dead to find his great-great-grandfather, a legendary singer.",
+	8.4,
+	"https://www.imdb.com/title/tt2380307/",
+	"https://www.imdb.com/title/tt2380307/mediaviewer/rm585455872",
+	2
+	);
+
+	INSERT INTO movies
+	VALUES (10,
+	"Toy Story",
+	1995,
+	"A cowboy doll is profoundly threatened and jealous when a new spaceman figure
+	supplants him as top toy in a boy's room.",
+	8.3,
+	"https://www.imdb.com/title/tt0114709/",
+	"https://www.imdb.com/title/tt0114709/mediaviewer/rm3813007616",
+	2
+	);
+
+	INSERT INTO movies
+	VALUES (11,
+	"Up",
+	2009,
+	"78-year-old Carl Fredricksen travels to Paradise Falls in his house equipped with
+	balloons, inadvertently taking a young stowaway.",
+	8.2,
+	"https://www.imdb.com/title/tt1049413/",
+	"https://www.imdb.com/title/tt1049413/mediaviewer/rm2582959360",
+	2
+	);
+
+    INSERT INTO movies
+	VALUES (12,
+	"How to Train Your Dragon",
+	2010,
+	"A hapless young Viking who aspires to hunt dragons becomes the unlikely friend of
+	a young dragon himself, and learns there may be more to the creatures than he assumed.",
+	8.1,
+	"https://www.imdb.com/title/tt0892769/",
+	"https://www.imdb.com/title/tt0892769/mediaviewer/rm2210040832",
+	2
+	);
+
+    INSERT INTO movies
+	VALUES (13,
+	"Finding Nemo",
+	2003,
+	"After his son is captured in the Great Barrier Reef and taken to Sydney, a timid clownfish
+	sets out on a journey to bring him home.",
+	8.1,
+	"https://www.imdb.com/title/tt0266543/",
+	"https://www.imdb.com/title/tt0266543/mediaviewer/rm3313243136",
+	2
+	);
+
+	INSERT INTO movies
+	VALUES (14,
+	"WALL-E",
+	2008,
+	"In the distant future, a small waste-collecting robot inadvertently embarks
+	on a space journey that will ultimately decide the fate of mankind.",
+	8.4,
+	"https://www.imdb.com/title/tt0910970/",
+	"https://www.imdb.com/title/tt0910970/mediaviewer/rm1659211008",
+	2
+	);
+
+	INSERT INTO movies
+	VALUES (15,
+	"The Intouchables",
+	2011,
+	"After he becomes a quadriplegic from a paragliding accident, an aristocrat
+	hires a young man from the projects to be his caregiver.",
+	8.5,
+	"https://www.imdb.com/title/tt1675434/",
+	"https://www.imdb.com/title/tt1675434/mediaviewer/rm1305720576",
+	3
+	);
+
+	INSERT INTO movies
+	VALUES (16,
+	"The Chaos Class",
+	1975,
+	"Lazy, uneducated students share a very close bond. They live together in the
+	dormitory, where they plan their latest pranks. When a new headmaster arrives,
+	the students naturally try to overthrow him. A comic war of nitwits follows.",
+	9.3,
+	"https://www.imdb.com/title/tt0252487/",
+	"https://www.imdb.com/title/tt0252487/mediaviewer/rm1915414272",
+	3
+	);
+
+	INSERT INTO movies
+	VALUES (17,
+	"Life is Beautiful",
+	1997,
+	"When an open-minded Jewish librarian and his son become victims of the Holocaust,
+	he uses a perfect mixture of will, humor, and imagination to protect his son from
+	the dangers around their camp.",
+	8.6,
+	"https://www.imdb.com/title/tt0118799/",
+	"https://www.imdb.com/title/tt0118799/mediaviewer/rm2303464448",
+	3
+	);
+
+	INSERT INTO movies
+	VALUES (18,
+	"Life is Beautiful",
+	1997,
+	"When an open-minded Jewish librarian and his son become victims of the Holocaust,
+	he uses a perfect mixture of will, humor, and imagination to protect his son from
+	the dangers around their camp.",
+	8.6,
+	"https://www.imdb.com/title/tt0118799/",
+	"https://www.imdb.com/title/tt0118799/mediaviewer/rm2303464448",
+	3
+	);
+
+	INSERT INTO movies
+	VALUES (19,
+	"The Truman Show",
+	1998,
+	"An insurance salesman discovers his whole life is actually a reality TV show.",
+	8.1,
+	"https://www.imdb.com/title/tt0120382/",
+	"https://www.imdb.com/title/tt0120382/mediaviewer/rm1927354112",
+	3
+	);
+
+	INSERT INTO movies
+	VALUES (20,
+	"Groundhog Day",
+	1993,
+	"A weatherman finds himself inexplicably living the same day over and over again.",
+	8.0,
+	"https://www.imdb.com/title/tt0107048/",
+	"https://www.imdb.com/title/tt0107048/mediaviewer/rm4118549504",
+	3
+	);
+
+	INSERT INTO movies
+	VALUES (21,
+	"The Godfather",
+	1972,
+	"The aging patriarch of an organized crime dynasty transfers control of his
+	clandestine empire to his reluctant son.",
+	9.2,
+	"https://www.imdb.com/title/tt0068646/",
+	"https://www.imdb.com/title/tt0068646/mediaviewer/rm746868224",
+	4
+	);
+
+	INSERT INTO movies
+	VALUES (22,
+	"Forrest Gump",
+	1994,
+	"The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate,
+	and other historical events unfold through the perspective of an Alabama man
+	with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
+	8.8,
+	"https://www.imdb.com/title/tt0109830/",
+	"https://www.imdb.com/title/tt0109830/mediaviewer/rm1954748672",
+	4
+	);
+
+	INSERT INTO movies
+	VALUES (23,
+	"Fight Club",
+	1999,
+	"An insomniac office worker and a devil-may-care soapmaker form an underground fight club
+	that evolves into something much, much more.",
+	8.8,
+	"https://www.imdb.com/title/tt0137523/",
+	"https://www.imdb.com/title/tt0137523/mediaviewer/rm1412004864",
+	4
+	);
+
+	INSERT INTO movies
+	VALUES (24,
+	"Goodfellas",
+	1990,
+	"The story of Henry Hill and his life in the mob, covering his relationship with his wife
+	Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.",
+	8.7,
+	"https://www.imdb.com/title/tt0099685/",
+	"https://www.imdb.com/title/tt0099685/mediaviewer/rm2091797760",
+	4
+	);
+
+	INSERT INTO movies
+	VALUES (25,
+	"1917",
+	2019,
+	"April 6th, 1917. As a regiment assembles to wage war deep in enemy territory, two soldiers are assigned
+	to race against time and deliver a message that will stop 1,600 men from walking straight into a deadly trap.",
+	8.4,
+	"https://www.imdb.com/title/tt8579674/",
+	"https://www.imdb.com/title/tt8579674/mediaviewer/rm144738817",
+	4
+	);
+
+	INSERT INTO movies
+	VALUES (26,
+	"Eternal Sunshine of the Spotless Mind",
+	2004,
+	"When their relationship turns sour, a couple undergoes a medical procedure to
+	have each other erased from their memories.",
+	8.3,
+	"https://www.imdb.com/title/tt0338013/",
+	"https://www.imdb.com/title/tt0338013/mediaviewer/rm2954530560",
+	4
+	);
+
+	INSERT INTO movies
+	VALUES (27,
+	"The Lord of the Rings: The Return of the King",
+	2003,
+	"Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze
+	from Frodo and Sam as they approach Mount Doom with the One Ring.",
+	8.9,
+	"https://www.imdb.com/title/tt0167260/",
+	"https://www.imdb.com/title/tt0167260/mediaviewer/rm584928512",
+	5
+	);
+
+	INSERT INTO movies
+	VALUES (28,
+	"The Lord of the Rings: The Fellowship of the Ring",
+	2001,
+	"A meek Hobbit from the Shire and eight companions set out on a journey to destroy
+	the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
+	8.8,
+	"https://www.imdb.com/title/tt0120737/",
+	"https://www.imdb.com/title/tt0120737/mediaviewer/rm3592958976",
+	5
+	);
+
+	INSERT INTO movies
+	VALUES (29,
+	"
+	Star Wars: Episode V - The Empire Strikes Back",
+	1980,
+	"After the Rebels are brutally overpowered by the Empire on the ice planet Hoth, Luke
+	Skywalker begins Jedi training with Yoda, while his friends are pursued by Darth Vader
+	and a bounty hunter named Boba Fett all over the galaxy.",
+	8.7,
+	"https://www.imdb.com/title/tt0080684/",
+	"https://www.imdb.com/title/tt0080684/mediaviewer/rm3114097664",
+	5
+	);
+
+	INSERT INTO movies
+	VALUES (30,
+	"Star Wars: Episode IV - A New Hope",
+	1997,
+	"Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids
+	to save the galaxy from the Empire's world-destroying battle station, while also attempting
+	to rescue Princess Leia from the Darth Vader.",
+	8.6,
+	"https://www.imdb.com/title/tt0076759/",
+	"https://www.imdb.com/title/tt0076759/mediaviewer/rm3263717120",
+	5
+	);
+
+	INSERT INTO movies
+	VALUES (31,
+	"Harry Potter and the Deathly Hallows: Part 2",
+	2011,
+	"Harry, Ron, and Hermione search for Voldemort's remaining Horcruxes in their
+	effort to destroy the Dark Lord as the final battle rages on at Hogwarts.",
+	8.1,
+	"https://www.imdb.com/title/tt1201607/",
+	"https://www.imdb.com/title/tt1201607/mediaviewer/rm1375082496",
+	5
+	);
+
+	INSERT INTO movies
+	VALUES (32,
+	"Pirates of the Caribbean: The Curse of the Black Pearl ",
+	2003,
+	"Blacksmith Will Turner teams up with eccentric pirate Captain Jack Sparrow
+	to save his love, the governor's daughter, from Jack's former pirate allies, who are now undead.",
+	8.0,
+	"https://www.imdb.com/title/tt0325980/",
+	"https://www.imdb.com/title/tt0325980/mediaviewer/rm2487103488",
+	5
+	);
+
+	INSERT INTO movies
+	VALUES (33,
+	"The Nightmare Before Christmas",
+	1993,
+	"Jack Skellington, king of Halloween Town, discovers Christmas Town, but his attempts to bring Christmas
+	to his home causes confusion.",
+	8.0,
+	"https://www.imdb.com/title/tt0107688/",
+	"https://www.imdb.com/title/tt0107688/mediaviewer/rm2164460544",
+	5
+	);
+
+	INSERT INTO movies
+	VALUES (34,
+	"Inception",
+	2010,
+	"A thief who steals corporate secrets through the use of dream-sharing technology is
+	given the inverse task of planting an idea into the mind of a C.E.O.",
+	8.8,
+	"https://www.imdb.com/title/tt1375666/",
+	"https://www.imdb.com/title/tt1375666/mediaviewer/rm3426651392",
+	6
+	);
+
+	INSERT INTO movies
+	VALUES (35,
+	"Interstellar",
+	2014,
+	"A team of explorers travel through a wormhole in space in an attempt to ensure
+	humanity's survival.",
+	8.6,
+	"https://www.imdb.com/title/tt0816692/",
+	"https://www.imdb.com/title/tt0816692/mediaviewer/rm4043724800",
+	6
+	);
+
+	INSERT INTO movies
+	VALUES (36,
+	"Parasite",
+	2019,
+	"A poor family, the Kims, con their way into becoming the servants of a rich family,
+	the Parks. But their easy life gets complicated when their deception is threatened with exposure.",
+	8.6,
+	"https://www.imdb.com/title/tt6751668/",
+	"https://www.imdb.com/title/tt6751668/mediaviewer/rm3194916865",
+	6
+	);
+
+	INSERT INTO movies
+	VALUES (37,
+	"Joker",
+	2019,
+	"In Gotham City, mentally troubled comedian Arthur Fleck is disregarded and mistreated by society. He
+	then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face
+	with his alter-ego: the Joker.",
+	8.5,
+	"https://www.imdb.com/title/tt7286456/",
+	"https://www.imdb.com/title/tt7286456/mediaviewer/rm3353122305",
+	6
+	);
+
+	INSERT INTO movies
+	VALUES (38,
+	"V For Vendetta",
+	2005,
+	"In a future British tyranny, a shadowy freedom fighter, known
+	only by the alias of V, plots to overthrow it with the help of a young woman.",
+	8.2,
+	"https://www.imdb.com/title/tt0434409/",
+	"https://www.imdb.com/title/tt0434409/mediaviewer/rm1691979008",
+	6
+	);
+
+	INSERT INTO movies
+	VALUES (39,
+	"Kill Bill: Vol. 1",
+	2003,
+	"After awakening from a four-year coma, a former assassin wreaks
+	vengeance on the team of assassins who betrayed her.",
+	8.1,
+	"https://www.imdb.com/title/tt0266697/",
+	"https://www.imdb.com/title/tt0266697/mediaviewer/rm2033140224",
+	6
+	);
+
