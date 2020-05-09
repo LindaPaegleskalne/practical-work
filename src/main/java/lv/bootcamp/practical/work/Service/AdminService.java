ddd19d96d3cdf5589e0c75bec5cfe18abaca1145
KriTil <tiltina.kristine@inbox.lv>
2020-05-09 13:17:54 +0300

    adminService methods

--- src/main/java/lv/bootcamp/practical/work/Service/AdminService.java
+++ src/main/java/lv/bootcamp/practical/work/Service/AdminService.java
@@ -1,12 +1,24 @@
 package lv.bootcamp.practical.work.Service;
 
+import lv.bootcamp.practical.work.movies.Category;
 import lv.bootcamp.practical.work.movies.CategoryRepository;
+import lv.bootcamp.practical.work.movies.Movie;
 import lv.bootcamp.practical.work.movies.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import java.util.Optional;
 
 @Service
 public class AdminService {
+    private static final Integer DEFAULT_PAGE_NR = 1;
+    private static final Integer DEFAULT_PAGE_SIZE = 5;
+
     private final MovieRepository movieRepository;
     private final CategoryRepository categoryRepository;
 
@@ -16,6 +28,53 @@ public class AdminService {
         this.categoryRepository = categoryRepository;
     }
 
+    public Iterable<Category> findAllCategory() {
+        return categoryRepository.findAll();
+    }
+
+    public Category findByIdCategory(Integer id) {
+        return categoryRepository.findById(id).
+                orElseThrow(() -> new IllegalArgumentException("Invalid category ID: "+ id));
+    }
+
+    public Category createCategory(Category category) {
+        categoryRepository.save(category);
+        return category;
+    }
+
+    public Category deleteCategory(Category category) {
+        categoryRepository.delete(category);
+        return category;
+    }
 
 
+//    public Iterable<Movie> findAllMovie(String search, Optional<Integer> page) {
+//        return movieRepository.findAll();
+//    }
+
+//    public Page<Movie> findByNameMovie(String search, Pageable pageable) {
+//        return movieRepository.findByName(search, pageable);
+//    }
+
+    public Page<Movie> startPage(String search, Model model, Optional<Integer> page) {
+        Pageable pageable = PageRequest.of(page.orElse(DEFAULT_PAGE_NR) - 1,
+                DEFAULT_PAGE_SIZE, Sort.by("name"));
+        model.addAttribute("searchStr", search);
+        return movieRepository.findByName(search, pageable);
+    }
+
+    public Movie findByIdMovie(Integer id) {
+        return movieRepository.findById(id).
+                orElseThrow(() -> new IllegalArgumentException("Invalid movie ID: "+ id));
+    }
+
+    public Movie createMovie(Movie movie) {
+        movieRepository.save(movie);
+        return movie;
+    }
+
+    public Movie deleteMovie(Movie movie) {
+        movieRepository.delete(movie);
+        return movie;
+    }
 }
--- src/main/java/lv/bootcamp/practical/work/movies/AdminController.java
+++ src/main/java/lv/bootcamp/practical/work/movies/AdminController.java
@@ -1,12 +1,10 @@
 package lv.bootcamp.practical.work.movies;
 
+import lv.bootcamp.practical.work.Service.AdminService;
 import lv.bootcamp.practical.work.movies.omdb.OmdbMovie;
 import lv.bootcamp.practical.work.movies.omdb.OmdbResponse;
 import lv.bootcamp.practical.work.movies.omdb.OmdbSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,29 +18,21 @@ import java.util.Optional;
 @Controller
 public class AdminController {
 
-    private static final Integer DEFAULT_PAGE_NR = 1;
-    private static final Integer DEFAULT_PAGE_SIZE = 5;
-
-    public final MovieRepository movieRepository;
-    public final CategoryRepository categoryRepository;
+    private final AdminService adminService;
     private OmdbSearchService omdbSearchService;
     private OmdbResponse omdbResponse;
 
     @Autowired
-    public AdminController(MovieRepository movieRepository, CategoryRepository categoryRepository, OmdbSearchService omdbSearchService) {
-        this.movieRepository = movieRepository;
-        this.categoryRepository = categoryRepository;
+    public AdminController(AdminService adminService, OmdbSearchService omdbSearchService) {
+        this.adminService = adminService;
         this.omdbSearchService = omdbSearchService;
     }
 
     @GetMapping("/admin")
     public String admin(@RequestParam(required = false) String search, Model model,
                         @RequestParam(required = false) Optional<Integer> page){
-        model.addAttribute("categories", categoryRepository.findAll());
-        Pageable pageable = PageRequest.of(page.orElse(DEFAULT_PAGE_NR) - 1,
-                DEFAULT_PAGE_SIZE, Sort.by("name"));
-        model.addAttribute("movies", movieRepository.findByName(search, pageable));
-        model.addAttribute("searchStr", search);
+        model.addAttribute("categories", adminService.findAllCategory());
+        model.addAttribute("movies", adminService.startPage(search, model, page));
         return "admin/index";
     }
 
@@ -52,77 +42,85 @@ public class AdminController {
     }
 
     @PostMapping("/admin/addcategory")
-    public String addCategory(@Valid Category category, Model model){
-        categoryRepository.save(category);
-        model.addAttribute("categories", categoryRepository.findAll());
-        model.addAttribute("movies", movieRepository.findAll());
+    public String addCategory(@Valid Category category, Model model,
+                              @RequestParam(required = false) String search,
+                             @RequestParam(required = false) Optional<Integer> page){
+        adminService.createCategory(category);
+        model.addAttribute("movies", adminService.startPage(search, model, page));
+        model.addAttribute("categories", adminService.findAllCategory());
         return "admin/index";
     }
 
     @GetMapping("/admin/editcategory/{id}")
     public String showUpdateFormCategory(@PathVariable("id") int id, Model model){
-        Category category =  categoryRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid category ID: "+ id));
+        Category category =  adminService.findByIdCategory(id);
         model.addAttribute("category", category);
         return "admin/update-category";
     }
 
     @PostMapping("/admin/updatecategory/{id}")
-    public String updateCategory (@PathVariable("id") int id, @Valid Category category, Model model) {
-        categoryRepository.save(category);
-        model.addAttribute("categories", categoryRepository.findAll());
-        model.addAttribute("movies", movieRepository.findAll());
+    public String updateCategory (@PathVariable("id") int id, @Valid Category category, Model model,
+                                  @RequestParam(required = false) String search,
+                                  @RequestParam(required = false) Optional<Integer> page) {
+        adminService.createCategory(category);
+        model.addAttribute("movies", adminService.startPage(search, model, page));
+        model.addAttribute("categories", adminService.findAllCategory());
         return "admin/index";
     }
 
     @GetMapping("/admin/deletecategory/{id}")
-    public String deleteCategory(@PathVariable("id") int id, Model model){
-        Category category = categoryRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid category ID: "+ id));
-        categoryRepository.delete(category);
-        model.addAttribute("categories", categoryRepository.findAll());
-        model.addAttribute("movies", movieRepository.findAll());
+    public String deleteCategory(@PathVariable("id") int id, Model model,
+                                 @RequestParam(required = false) String search,
+                                 @RequestParam(required = false) Optional<Integer> page){
+        Category category = adminService.findByIdCategory(id);
+        adminService.deleteCategory(category);
+        model.addAttribute("movies", adminService.startPage(search, model, page));
+        model.addAttribute("categories", adminService.findAllCategory());
         return "admin/index";
     }
 
     @GetMapping("/admin/signupmovie")
     public String showSignUpFormMovie(Movie movie, Model model){
-        model.addAttribute("categories", categoryRepository.findAll());
+        model.addAttribute("categories", adminService.findAllCategory());
         return "admin/add-movie";
     }
 
     @PostMapping("/admin/addmovie")
-    public String addMovie(@Valid Movie movie, Model model){
-        movieRepository.save(movie);
-        model.addAttribute("movies", movieRepository.findAll());
-        model.addAttribute("categories", categoryRepository.findAll());
+    public String addMovie(@Valid Movie movie, Model model,
+                           @RequestParam(required = false) String search,
+                           @RequestParam(required = false) Optional<Integer> page){
+        adminService.createMovie(movie);
+        model.addAttribute("movies", adminService.startPage(search, model, page));
+        model.addAttribute("categories", adminService.findAllCategory());
         return "admin/index";
     }
 
     @GetMapping("/admin/editmovie/{id}")
-    public String showUpdateFormMovie(@PathVariable("id") int id, Model model){
-        Movie movie = movieRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid movie ID: "+ id));
+    public String showUpdateFormMovie(@PathVariable("id") Integer id, Model model){
+        Movie movie = adminService.findByIdMovie(id);
         model.addAttribute("movie", movie);
-        model.addAttribute("categories", categoryRepository.findAll());
+        model.addAttribute("categories", adminService.findAllCategory());
         return "admin/update-movie";
     }
 
     @PostMapping("/admin/updatemovie/{id}")
-    public String updateMovie (@PathVariable("id") int id, @Valid Movie movie, Model model) {
-        movieRepository.save(movie);
-        model.addAttribute("movies", movieRepository.findAll());
-        model.addAttribute("categories", categoryRepository.findAll());
+    public String updateMovie (@PathVariable("id") Integer id, @Valid Movie movie, Model model,
+                               @RequestParam(required = false) String search,
+                               @RequestParam(required = false) Optional<Integer> page) {
+        adminService.createMovie(movie);
+        model.addAttribute("movies", adminService.startPage(search, model, page));
+        model.addAttribute("categories", adminService.findAllCategory());
         return "admin/index";
     }
 
     @GetMapping("/admin/deletemovie/{id}")
-    public String deleteMovie(@PathVariable("id") int id, Model model){
-        Movie movie = movieRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid movie ID: "+ id));
-        movieRepository.delete(movie);
-        model.addAttribute("movies", movieRepository.findAll());
-        model.addAttribute("categories", categoryRepository.findAll());
+    public String deleteMovie(@PathVariable("id") Integer id, Model model,
+                              @RequestParam(required = false) String search,
+                              @RequestParam(required = false) Optional<Integer> page){
+        Movie movie = adminService.findByIdMovie(id);
+        adminService.deleteMovie(movie);
+        model.addAttribute("movies", adminService.startPage(search, model, page));
+        model.addAttribute("categories", adminService.findAllCategory());
         return "admin/index";
     }
 
@@ -137,15 +135,17 @@ public class AdminController {
     public String showUpdateFormOmdbMovie(@PathVariable("title") String title, Model model){
         OmdbMovie omdbMovie = omdbResponse.searchByTitle(title);
         model.addAttribute("movie", omdbMovie.addOmdbMovie());
-        model.addAttribute("categories", categoryRepository.findAll());
+        model.addAttribute("categories", adminService.findAllCategory());
         return "admin/add-omdb-movie";
     }
 
     @PostMapping("/admin/addomdbmovie/{title}")
-    public String addOmdbMovie ( @PathVariable("title") String title, @Valid Movie movie, Model model) {
-        movieRepository.save(movie);
-        model.addAttribute("movies", movieRepository.findAll());
-        model.addAttribute("categories", categoryRepository.findAll());
+    public String addOmdbMovie ( @PathVariable("title") String title, @Valid Movie movie, Model model,
+                                 @RequestParam(required = false) String search,
+                                 @RequestParam(required = false) Optional<Integer> page) {
+        adminService.createMovie(movie);
+        model.addAttribute("movies", adminService.startPage(search, model, page));
+        model.addAttribute("categories", adminService.findAllCategory());
         return "admin/index";
     }
 }
