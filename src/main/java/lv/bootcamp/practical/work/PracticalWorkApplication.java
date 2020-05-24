bcec4497aaf75117273e7f93622a70e5255402bf
LaLapina <lapina.agnese@gmail.com>
2020-05-24 16:57:28 +0300

    Checkstyle violation corrections

--- src/main/java/lv/bootcamp/practical/work/PracticalWorkApplication.java
+++ src/main/java/lv/bootcamp/practical/work/PracticalWorkApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PracticalWorkApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PracticalWorkApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PracticalWorkApplication.class, args);
+    }
 }
